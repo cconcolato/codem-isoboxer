27f687c7290e46e419dd0e1c5faae3e58ddc62f9
cconcolato <cyril.concolato@telecom-paristech.fr>
2016-07-19 13:59:50 +0200

    adding Producer Reference Time Box for latency measurement in DASH.js

--- dist/iso_boxer.js
+++ dist/iso_boxer.js
@@ -383,6 +383,21 @@ ISOBox.prototype._boxParsers['payl'] = function() {
   this.cue_text = ISOBoxer.Utils.dataViewToString(cue_text_raw);
 }
 ;
+// ISO/IEC 14496-12:2015 - 8.6.15 Producer Reference Time Box
+ISOBox.prototype._boxParsers['prft'] = function() {
+	this._parseFullBox();
+	this.reference_track_ID = this._readUint(32);
+	// read as two separate 32-bits fields instead of a single 64-bits field to avoid JS 64-bit issues
+	this.ntp_timestamp_sec = this._readUint(32);
+	this.ntp_timestamp_frac = this._readUint(32);
+	if (this.version == 0) {
+		this.media_time = this._readUint(32);
+	} else {
+		this.media_time = this._readUint(64);
+	}
+};
+
+;
 // ISO/IEC 14496-12:2012 - 8.16.3 Segment Index Box
 ISOBox.prototype._boxParsers['sidx'] = function() {
   this._parseFullBox();
